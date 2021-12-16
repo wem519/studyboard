432eb39a950229f910323c0cd5fe1844808f5726
wem519 <wem519@gmail.com>
2021-12-16 13:33:42 +0900

    글 작성 페이지

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,20 +1,40 @@
-import { Input, Textarea } from '@chakra-ui/react'
+import { Input, Textarea } from "@chakra-ui/react";
 import styles from "../../styles/Write.module.css";
 
-import {useState} from "react"
+export default function Write() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <label>제목</label>
+        <Input className={styles.input} placeholder="제목" />
+      </div>
+      <div className={styles.wrapper}>
+        <label>작성자</label>
+        <Input className={styles.input} placeholder="작성자" />
+      </div>
+      <div className={styles.wrapper}>
+        <label>카테고리</label>
+        <Input className={styles.input} placeholder="카테고리" />
+      </div>
+      <div className={styles.wrapper}>
+        <label>인원</label>
+        <Input className={styles.input} placeholder="인원" />
+      </div>
+      <div className={styles.wrapper}>
+        <label>위치</label>
+        <Input className={styles.input} placeholder="위치" />
+      </div>
+      <div className={styles.wrapper}>
+        <label>기간</label>
+        <Input className={styles.half} placeholder="2021.12.15~" />
+        <Input className={styles.half} placeholder="2021.12.31" />
 
-export default function Write(){
-
-
-
-    return(
-        <div className={styles.container}>
-            <Input className={styles.input} placeholder='제목' />
-            <Input className={styles.input} placeholder='카테고리'/>
-            <Input className={styles.input} placeholder='인원'/>
-            <Input className={styles.input} placeholder='위치'/>
-            <Input className={styles.input} placeholder='기간 2021.12.15-2021.12.31'/>
-            <Textarea className={styles.input} placeholder="내용을 입력해주세요"/>
-        </div>
-    )
+      </div>
+      <div className={styles.wrapper}>
+        <label>상세 내용</label>
+        <Textarea className={styles.input} placeholder="내용을 입력해주세요" />
+      </div>
+    </div>
+    
+  );
 }
